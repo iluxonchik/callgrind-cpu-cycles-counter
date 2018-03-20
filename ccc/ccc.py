e29466b61f6361bb5bf7c2137518184dc0055720
ILUXONCHIK <iluxon4ik@hotmail.com>
2018-03-20 05:03:01 +0000

    implement get_cc_from_callgrind_file

--- ccc/ccc.py
+++ ccc/ccc.py
@@ -1,4 +1,5 @@
 import subprocess
+import re
 
 def parse_ciphersuite_list_from_file(file_path):
     """Parses a list of ciphersuites from a file.
@@ -12,9 +13,15 @@ def parse_ciphersuite_list_from_file(file_path):
     return ciphersuites
 
 def get_cc_from_callgrind_file(callgrind_file, func_name):
-    """Gets the number of CPU cycles for a function from a callgrind file.
-    """
-    pass
+    """Gets the number of CPU cycles for a function from a callgrind file."""
+    with open(callgrind_file, 'r') as f:
+        file_content = f.read()
+
+    REGEX = fr'{func_name}\n.+\n\* (?P<cpu_cycles>\d+)'
+    pattern = re.compile(REGEX)
+    res = pattern.search(file_content)
+    return int(res.group('cpu_cycles'))
+
 
 def run_server(ciphersuite_id):
     args = ['../../mbed-tls-playground/mbedtls-2.7.0/playground/server_cc', str(ciphersuite_id)]
