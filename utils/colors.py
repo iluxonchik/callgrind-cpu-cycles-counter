4d9c74ae5cb9270323aeda7bcc935b9976afbd3c
Illya Gerasymchuk <illya@iluxonchik.me>
2018-10-06 03:32:17 +0100

    add print with colors util

---
+++ utils/colors.py
@@ -0,0 +1,13 @@
+"""Module containing color printing"""
+
+def print_red(text, end='\n'):
+    print(f'\u001b[1m\u001b[31m{text}\u001b[0m', end=end)
+
+def print_green(text, end='\n'):
+    print(f'\u001b[1m\u001b[32m{text}\u001b[0m', end=end)
+
+def print_yellow(text, end='\n'):
+    print(f'\u001b[33m\u001b[33m{text}\u001b[0m', end=end)
+
+def print_magenta(text, end='\n'):
+    print(f'\u001b[35m\u001b[31m{text}\u001b[0m', end=end)
